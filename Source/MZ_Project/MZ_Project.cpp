bed21fa5461636a69de8a11f2077f97562a8c421
hyojinkim830 <20181850@office.bufs.ac.kr>
2025-03-25 00:20:45 +0900

    03_25_DevelopmentFirstpush(김효진)

---
+++ Source/MZ_Project/MZ_Project.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MZ_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MZ_Project, "MZ_Project" );
